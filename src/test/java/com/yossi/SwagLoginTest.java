e935e09c66735a722e15dbac1b7fa449b999f700
yossiamb007 <lygagrealestate@gmail.com>
2025-04-16 16:12:39 +0300

    Home exam

--- src/test/java/com/yossi/SwagLoginTest.java
+++ src/test/java/com/yossi/SwagLoginTest.java
@@ -6,14 +6,14 @@ import org.testng.annotations.Test;
 
 public class SwagLoginTest extends TestBase{
     private final String SWAG_PAGE_URL = "https://www.saucedemo.com/";
-    //    @Test
-//    public void testLoginSuccess(){
-//        webAction.getUrl(wikiUITestAutoUrl);
-//        SwagLoginPage loginPage = new SwagLoginPage(webAction);
-//
-//        String result = loginPage.login("standard_user", "secret_sauce");
-//        Assert.assertEquals(result, "Swag Labs", "Expected successful login");Ddaus@paloaltonetworks.com
-//    }
+        @Test
+    public void testLoginSuccess(){
+        webAction.getUrl(SWAG_PAGE_URL);
+        SwagLoginPage loginPage = new SwagLoginPage(webAction);
+
+        String result = loginPage.login("standard_user", "secret_sauce");
+        Assert.assertEquals(result, "Swag Labs", "Expected successful login");
+    }
     @Test
     public void testFilter(){
         webAction.getUrl(SWAG_PAGE_URL);
